e6d5ba8c5c22dcb3899422f627588c4138ef467e
Firoz kumar <56572016+Firoz-kumar-3@users.noreply.github.com>
2021-02-17 21:46:44 +0530

    Update dijkastra_algoritm.cpp

--- dijkastra_algoritm.cpp
+++ dijkastra_algoritm.cpp
@@ -68,3 +68,56 @@ vector <int> dijkstra(vector<vector<int>> g, int src, int V)
    //return v;
    
 }
+// another method :
+
+
+class Solution{
+	public:
+	/*  Function to implement Dijkstra
+    *   adj: vector of vectors which represents the graph
+    *   S: source vertex to start traversing graph with
+    *   V: number of vertices
+    */
+    vector <int> dijkstra(int V, vector<vector<int>> m[], int S) {
+        // Code here
+
+    int dist[V+2];
+    for(int i=0;i<V;i++)
+    {
+        dist[i]=INT_MAX;
+    }
+    priority_queue<pair<int,int> ,vector<pair<int,int>>,greater<pair<int,int> > > pq;
+    
+    pq.push(make_pair(0,S));
+    dist[S]=0;
+    
+    while(!pq.empty())
+    {
+        auto curr=pq.top();
+        int cur_dis=curr.first;
+        int cur_node=curr.second;
+        pq.pop();
+        for(auto nbr : m[cur_node])
+        {
+            if(cur_dis+nbr[1]<dist[nbr[0]])
+            {
+              
+                dist[nbr[0]]=cur_dis+nbr[1];
+                pq.push(make_pair(dist[nbr[0]],nbr[0]));
+            }
+        }
+   }
+vector<int> v;
+for(int i=0;i<V;i++)
+{
+    v.push_back(dist[i]);
+}
+return v;
+}
+
+
+};
+
+
+
+
